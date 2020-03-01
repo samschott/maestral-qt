a275c25b40f184a297a2db76ebeec2bd1f8a2c0c
Sam Schott <ss2151@cam.ac.uk>
2020-03-01 17:09:11 +0000

    bug fix when clearing sync error

--- maestral_qt/__init__.py
+++ maestral_qt/__init__.py
@@ -8,7 +8,7 @@ Created on Wed Oct 31 16:23:13 2018
 import os
 
 __author__ = 'Sam Schott'
-__version__ = '0.6.2.dev1'
+__version__ = '0.6.2.dev2'
 __url__ = 'https://github.com/SamSchott/maestral'
 
 # add "~/.local/share" to XDG_DATA_DIRS
