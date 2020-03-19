78e80b9be0717f4ff9baf1fe298fe90b9d38a8df
Sam Schott <ss2151@cam.ac.uk>
2020-03-19 17:09:02 +0000

    released post1

--- maestral_qt/__init__.py
+++ maestral_qt/__init__.py
@@ -8,7 +8,7 @@ Created on Wed Oct 31 16:23:13 2018
 import os
 
 __author__ = 'Sam Schott'
-__version__ = '0.6.2'
+__version__ = '0.6.2.post1'
 __url__ = 'https://github.com/SamSchott/maestral'
 
 # add "~/.local/share" to XDG_DATA_DIRS
