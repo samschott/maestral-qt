b1b50a116f47c4426150c9857a7c56886e177426
Sam Schott <ss2151@cam.ac.uk>
2020-04-09 17:14:33 +0200

    refined appearance of relink dialog

--- maestral_qt/relink_dialog.py
+++ maestral_qt/relink_dialog.py
@@ -19,9 +19,10 @@ from .utils import MaestralBackgroundTask
 class RelinkDialog(QtWidgets.QDialog):
     """A dialog to show when Maestral's Dropbox access has expired or has been revoked."""
 
-    VALID_MSG = "Verified. Restarting Maestral..."
-    INVALID_MSG = "Invalid token"
-    CONNECTION_ERR_MSG = "Connection failed"
+    VALID_MSG = 'Verified. Restarting Maestral...'
+    INVALID_MSG = 'Invalid token'
+    CONNECTION_ERR_MSG = 'Connection failed'
+    PLACEHOLDER_TEXT = 'Authorization token'
 
     EXPIRED = 0
     REVOKED = 1
@@ -30,24 +31,24 @@ class RelinkDialog(QtWidgets.QDialog):
         super().__init__()
         uic.loadUi(RELINK_DIALOG_PATH, self)
         # noinspection PyTypeChecker
-        self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint
-                            | Qt.WindowTitleHint | Qt.CustomizeWindowHint)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.Sheet | Qt.WindowTitleHint
+                            | Qt.CustomizeWindowHint)
 
         self._parent = parent
         self.mdbx = self._parent.mdbx
 
         # format text labels
         if reason is self.EXPIRED:
-            self.titleLabel.setText("Dropbox Access Expired")
+            self.titleLabel.setText('Dropbox Access Expired')
             formatted_text = self.infoLabel.text().format(
-                "has expired", self.mdbx.get_auth_url())
+                'has expired', self.mdbx.get_auth_url())
         elif reason is self.REVOKED:
-            self.titleLabel.setText("Dropbox Access Revoked")
+            self.titleLabel.setText('Dropbox Access Revoked')
             formatted_text = self.infoLabel.text().format(
-                "has been revoked", self.mdbx.get_auth_url())
+                'has been revoked', self.mdbx.get_auth_url())
         else:
-            raise ValueError("'reason' must be RelinkDialog.EXPIRED or "
-                             "RelinkDialog.REVOKED.")
+            raise ValueError('"reason" must be RelinkDialog.EXPIRED or '
+                             'RelinkDialog.REVOKED.')
         self.infoLabel.setText(formatted_text)
         self.titleLabel.setFont(get_scaled_font(bold=True))
         self.infoLabel.setFont(get_scaled_font(scaling=0.9))
@@ -61,7 +62,7 @@ class RelinkDialog(QtWidgets.QDialog):
         self.lineEditAuthCode.setTextMargins(3, 0, 0, 0)
 
         # connect callbacks
-        self.lineEditAuthCode.textChanged.connect(self._set_text_style)
+        self.lineEditAuthCode.textChanged.connect(self._update_appearance)
         self.pushButtonCancel.clicked.connect(self._parent.quit)
         self.pushButtonUnlink.clicked.connect(self.delete_creds_and_quit)
         self.pushButtonLink.clicked.connect(self.on_link_clicked)
@@ -76,29 +77,31 @@ class RelinkDialog(QtWidgets.QDialog):
         self._parent.quit(stop_daemon=True)
 
     @QtCore.pyqtSlot(str)
-    def _set_text_style(self, text):
-        if text == "":
-            self.pushButtonLink.setEnabled(False)
-            self.lineEditAuthCode.setStyleSheet("")
-        elif text == self.INVALID_MSG:
-            self.pushButtonLink.setEnabled(False)
-            self.lineEditAuthCode.setStyleSheet("color: rgb(205, 0, 0); font: bold;")
-        elif text == self.CONNECTION_ERR_MSG:
-            self.pushButtonLink.setEnabled(False)
-            self.lineEditAuthCode.setStyleSheet("color: rgb(205, 0, 0); font: bold;")
-        elif text == self.VALID_MSG:
-            self.pushButtonLink.setEnabled(False)
-            self.pushButtonUnlink.setEnabled(False)
-            self.pushButtonCancel.setEnabled(False)
-            self.lineEditAuthCode.setStyleSheet("color: rgb(0, 129, 0); font: bold;")
+    def _update_appearance(self, text):
+        placeholder_text = self.lineEditAuthCode.placeholderText()
+        if text == '':
+            if placeholder_text == self.PLACEHOLDER_TEXT:
+                self.pushButtonLink.setEnabled(False)
+                self.lineEditAuthCode.setStyleSheet('')
+            elif placeholder_text == self.INVALID_MSG:
+                self.pushButtonLink.setEnabled(False)
+                self.lineEditAuthCode.setStyleSheet('color: rgb(205, 0, 0); font: bold;')
+            elif placeholder_text == self.CONNECTION_ERR_MSG:
+                self.pushButtonLink.setEnabled(False)
+                self.lineEditAuthCode.setStyleSheet('color: rgb(205, 0, 0); font: bold;')
+            elif placeholder_text == self.VALID_MSG:
+                self.pushButtonLink.setEnabled(False)
+                self.pushButtonUnlink.setEnabled(False)
+                self.pushButtonCancel.setEnabled(False)
+                self.lineEditAuthCode.setStyleSheet('color: rgb(0, 129, 0); font: bold;')
         else:
             self.pushButtonLink.setEnabled(True)
-            self.lineEditAuthCode.setStyleSheet("")
+            self.lineEditAuthCode.setStyleSheet('')
 
     @QtCore.pyqtSlot()
     def on_link_clicked(self):
         token = self.lineEditAuthCode.text()
-        if token == "":
+        if token == '':
             # this should not occur because link button will be inactivate when there
             # is no text in QLineEdit
             return
@@ -117,15 +120,17 @@ class RelinkDialog(QtWidgets.QDialog):
     def on_link_done(self, res):
 
         if res == 0:
-            self.lineEditAuthCode.setText(self.VALID_MSG)
+            self.lineEditAuthCode.setPlaceholderText(self.VALID_MSG)
             QtWidgets.QApplication.processEvents()
             QtCore.QTimer.singleShot(200, self._parent.restart)
         elif res == 1:
-            self.lineEditAuthCode.setText(self.INVALID_MSG)
+            self.lineEditAuthCode.setPlaceholderText(self.INVALID_MSG)
             self.set_ui_idle()
         elif res == 2:
-            self.lineEditAuthCode.setText(self.CONNECTION_ERR_MSG)
+            self.lineEditAuthCode.setPlaceholderText(self.CONNECTION_ERR_MSG)
             self.set_ui_idle()
+
+        self.lineEditAuthCode.setText('')
 
     def set_ui_busy(self):
         self.progressIndicator.startAnimation()
