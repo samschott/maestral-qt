d09e7c9c8cbe9bf51e92f0c1f8413d31506efc1c
Sam Schott <ss2151@cam.ac.uk>
2020-05-12 16:32:05 +0200

    [main] simplify daemon startup

--- maestral_qt/main.py
+++ maestral_qt/main.py
@@ -29,7 +29,6 @@ from maestral.daemon import (
     start_maestral_daemon_process,
     start_maestral_daemon,
     stop_maestral_daemon_process,
-    get_maestral_pid,
     get_maestral_proxy,
     Start,
     Pyro5
@@ -182,22 +181,18 @@ class MaestralGuiApp(QtWidgets.QSystemTrayIcon):
 
     def get_or_start_maestral_daemon(self):
 
-        pid = get_maestral_pid(self.config_name)
-        if pid:
-            self._started = False
-        else:
-            res = start_maestral_daemon_process(self.config_name)
+        res = start_maestral_daemon_process(self.config_name)
 
-            if res == Start.Failed:
-                title = 'Could not start Maestral'
-                message = ('Could not start or connect to sync daemon. Please try again '
-                           'and contact the developer if this issue persists.')
-                show_dialog(title, message, level='error')
-                self.quit()
-            elif res == Start.AlreadyRunning:
-                self._started = False
-            elif res == Start.Ok:
-                self._started = True
+        if res == Start.Failed:
+            title = 'Could not start Maestral'
+            message = ('Could not start or connect to sync daemon. Please try again '
+                       'and contact the developer if this issue persists.')
+            show_dialog(title, message, level='error')
+            self.quit()
+        elif res == Start.AlreadyRunning:
+            self._started = False
+        elif res == Start.Ok:
+            self._started = True
 
         return get_maestral_proxy(self.config_name)
 
