9ac4add6c38fb5f888a4cc52f0ed3b2d8be136d5
Sam Schott <ss2151@cam.ac.uk>
2020-09-15 20:29:03 +0100

    bump to v1.2.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         'bugsnag>=3.4.0',
         'click>=7.1.1',
-        'maestral>=1.2.0.dev2',
+        'maestral>=1.2.0',
         'markdown2',
         'packaging',
         'PyQt5>=5.9',
