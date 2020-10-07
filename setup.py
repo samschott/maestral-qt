e72f638a3fab40571a5b43be7b1e941a1b0d7541
Sam Schott <ss2151@cam.ac.uk>
2020-10-07 22:38:52 +0100

    bump daemon requirement to v1.2.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         "bugsnag>=3.4.0",
         "click>=7.1.1",
-        "maestral>=1.2.0",
+        "maestral>=1.2.1",
         "markdown2",
         "packaging",
         "PyQt5>=5.9",
