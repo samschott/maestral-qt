a6e0572394a2746c974e8951ce3d17a19cf8d630
Sam Schott <ss2151@cam.ac.uk>
2020-10-07 22:41:54 +0100

    Bump version: 1.2.1.dev0 → 1.2.1

--- maestral_qt/__init__.py
+++ maestral_qt/__init__.py
@@ -8,7 +8,7 @@ Created on Wed Oct 31 16:23:13 2018
 import os
 
 __author__ = "Sam Schott"
-__version__ = "1.2.1.dev0"
+__version__ = "1.2.1"
 __url__ = "https://github.com/SamSchott/maestral"
 
 # add '~/.local/share' to XDG_DATA_DIRS
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="maestral-qt",
     author="Sam Schott",
     author_email="ss2151@cam.ac.uk",
-    version="v1.2.1.dev0",
+    version="v1.2.1",
     description="A Qt GUI for the Maestral daemon",
     url="https://github.com/SamSchott/maestral",
     license="MIT",
