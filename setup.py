9d62b6568f8ef86b5d9cc86cdc24dccb7f70038b
Sam Schott <ss2151@cam.ac.uk>
2021-03-09 18:20:11 +0000

    Bump version: 1.4.2 → 1.4.3.dev0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="maestral-qt",
     author="Sam Schott",
     author_email="ss2151@cam.ac.uk",
-    version="1.4.2",
+    version="1.4.3.dev0",
     description="A Qt GUI for the Maestral daemon",
     url="https://github.com/SamSchott/maestral",
     license="MIT",
