6df53398481f60f6f370afeed1f437767cc9855c
Sam Schott <ss2151@cam.ac.uk>
2021-11-26 13:27:39 +0100

    bump daemon requirement

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         "click>=7.1.1",
         "importlib_resources;python_version<'3.9'",
-        "maestral>=1.5.1",
+        "maestral>=1.5.2",
         "markdown2",
         "packaging",
         "PyQt5>=5.9",
